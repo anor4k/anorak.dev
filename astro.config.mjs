1d82d2a6903260a1d9a1897e5ae86c51fd85a4a8
Noel <noel.eliezer@gmail.com>
2024-10-16 21:28:24 -0300

    update some elements and icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://anorak.dev"
+  site: "https://anorak.dev",
+  integrations: [
+    icon({
+      include: {
+        hugeicons: ["mail-02", "github", "twitter"]
+      }
+    })
+  ]
 });
